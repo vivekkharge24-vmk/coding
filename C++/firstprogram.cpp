25967f44dd34b3c7e96458f999ec50643e0fa4b6
Vivek <vivekkharge24@gmail.com>
2026-02-08 20:12:28 +0530

    first commit

---
+++ C++/firstprogram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter two nuumber:";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum="<<sum<<endl;
+    return 0;
+}
---
+++ java/firstjavaprogram.java
@@ -0,0 +1,6 @@
+public class firstjavaprogram{
+    public static void main(String[]args) {
+        System.out.println("Hello word");
+        
+    }
+}
