215a705303657bb5e6a10ced7d241d1f2becc65f
Vivek <vivekkharge24@gmail.com>
2026-02-13 14:31:36 +0530

    logical operators program

---
+++ C/logical_operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a=0,b=4;
+    
+    printf("a && b %d\n",a&&b);
+    printf("a || b %d\n",a||b);
+    printf("a ! b %d\n",a!=b); // Not Equal to check
+    return 0;
+}
