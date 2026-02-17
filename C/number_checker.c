3b2b07b95c63b50c9b1e8ec264d866b78ba74ae2
Vivek <vivekkharge24@gmail.com>
2026-02-17 17:22:14 +0530

    Added odd and even number check program

---
+++ C/number_checker.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    if(num%2==0)
+    {
+        printf("%d Is Even Number ",num);
+    }
+    else
+    {
+        printf("%d Is Odd Number ",num);
+    }
+    return 0;
+}
