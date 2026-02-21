6ff5a83952255f73b57d6d29c6d31958e7287d90
Vivek <vivekkharge24@gmail.com>
2026-02-21 21:25:31 +0530

    Added sequare pattern program using loop

---
+++ C/loop_star.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+
+    for(i=1;i<=5;i++)  // First loop
+    {
+        for(j=1;j<=5;j++) // Second loop
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
