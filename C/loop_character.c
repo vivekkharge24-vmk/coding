04139f7c2bacc02998e1dec6f1398a7d67396832
Vivek <vivekkharge24@gmail.com>
2026-02-21 21:56:45 +0530

    Added character pattern program using loop

---
+++ C/loop_character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char i;
+
+     for(i='A';i<='E';i++)
+     {
+        for(int j=1;j<=5;j++)
+        {
+            printf("%c ",i);
+        }
+        printf("\n");
+     }
+}
