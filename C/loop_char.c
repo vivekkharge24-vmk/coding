da9c3635b2ad2201c857870fdc282996df5dfb2a
Vivek <vivekkharge24@gmail.com>
2026-02-21 21:54:55 +0530

    Added character a to x print star pattern program

---
+++ C/loop_char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{  
+   int i,j;
+   char c='A';
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            printf("%c ",c);
+            c++;
+        }
+        printf("\n");
+    }
+}
