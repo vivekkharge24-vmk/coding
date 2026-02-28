6819617792d78dde0b6f6c174e20ffe339961f50
Vivek <vivekkharge24@gmail.com>
2026-02-28 17:28:13 +0530

    Added traversal array program

---
+++ C/array_traversal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    //  Traversal Array Program
+    int arr[5]={1,2,3,4,5};
+     
+    printf("The Element Are : ");
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
