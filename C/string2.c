97b573e853b5d7d7f9dff7e18299b49d877869af
Vivek <vivekkharge24@gmail.com>
2026-03-05 20:20:27 +0530

    Added find length of string program

---
+++ C/string2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+// Find Length Of String Program
+int main()
+{
+    char a[10]={"vivek"};
+    int l = strlen(a);
+    printf("Length of given string : %d",l);
+}
