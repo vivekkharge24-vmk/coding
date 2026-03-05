0963c99ca558eeb8c2c32a0dab6e96432ae4a0dc
Vivek <vivekkharge24@gmail.com>
2026-03-05 20:18:30 +0530

    Added find length of without string program

---
+++ C/loop_length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+// Find Length Of Without String Program
+int main()
+{
+    char a[10]={"vivek"};
+    int i;
+    for(i=0;a[i]!='\0';i++){
+     }
+
+    printf("Length of given string : %d",i);
+}
