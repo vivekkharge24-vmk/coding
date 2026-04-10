ef53a6be10279e9b309d287639e4b0bed870c114
Vivek <vivekkharge24@gmail.com>
2026-04-10 17:15:14 +0530

    Added increment  operator program

---
+++ C++/increment_operators.cpp
@@ -0,0 +1,23 @@
+// Increment Operator Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5, r, a2 = 5, r2;
+
+    // Pre-increment
+    r = ++a;
+    cout << endl
+         << "======== Calculation ========" << endl;
+    cout << "result : " << r << endl;
+    cout << "value of a : " << a << endl;
+
+    // Post-increment
+    cout << endl
+         << "======== Calculation ========" << endl;
+    r2 = a2++;
+    cout << "result : " << r2 << endl;
+    cout << "Value of a : " << a2 << endl;
+
+    return 0;
+}
