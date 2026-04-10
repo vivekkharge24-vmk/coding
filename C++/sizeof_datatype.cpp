4dddcc0f2730066edaa3a7a27565b10256358367
Vivek <vivekkharge24@gmail.com>
2026-04-10 17:30:35 +0530

    Added sizeof data type program

---
+++ C++/sizeof_datatype.cpp
@@ -0,0 +1,12 @@
+// SizeOf Datatype Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "All Data Types Bytes Size" << endl;
+    cout << "int : " << sizeof(int) << endl;
+    cout << "char : " << sizeof(char) << endl;
+    cout << "float : " << sizeof(float) << endl;
+    cout << "double : " << sizeof(double) << endl;
+    cout << "string : " << sizeof(string) << endl;
+}
