569dcae028e342df8cf3b4e78693526fa42f0cc9
Vivek <vivekkharge24@gmail.com>
2026-04-10 17:13:25 +0530

    Added names print program

---
+++ C++/identifier.cpp
@@ -0,0 +1,15 @@
+// Two Names Print Program Using Function
+#include <iostream>
+using namespace std;
+
+void myname()
+{
+
+    cout << "gaurav" << endl;
+}
+int main()
+{
+    string name = "vivek";
+    cout << name << endl;
+    myname();
+}
