d0c3e95e83b9ad70c8ad563d7ac63b5d1c86f6b2
Vivek <vivekkharge24@gmail.com>
2026-04-10 17:32:17 +0530

    Added pre-defined datatype print program

---
+++ C++/pre_defined.cpp
@@ -0,0 +1,13 @@
+// Pre-defined Data Type Print Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    char abc = 'A';
+    string name = "vivek";
+    float num = 10.50;
+
+    cout << n << " " << abc << " " << name << " " << num;
+    return 0;
+}
