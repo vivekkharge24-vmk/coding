75432a16e61bf12d3aa4a279119c2b008e72c4fe
Vivek <vivekkharge24@gmail.com>
2026-04-13 16:12:23 +0530

    Added hollow diamond pattern program

---
+++ C++/hollow_diamond_pattern.cpp
@@ -0,0 +1,48 @@
+// Hollow Diamond Pattern Print Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    // Top diagram
+    cout << "Hollow Diamond pattern" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Bottom diagram
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
