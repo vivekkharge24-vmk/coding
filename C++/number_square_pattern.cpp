40fcaf97ad0369806cd7fd4e281ae0ec7d3b5270
Vivek <vivekkharge24@gmail.com>
2026-04-13 16:10:33 +0530

    Added number square pattern program

---
+++ C++/number_square_pattern.cpp
@@ -0,0 +1,15 @@
+// Number Square Pattern Print Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Number square pattern" << endl;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
