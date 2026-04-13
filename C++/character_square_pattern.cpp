e72935b9a8a510e71b96f8c64b97af453cfd9f87
Vivek <vivekkharge24@gmail.com>
2026-04-13 16:32:12 +0530

    Added character square pattern program

---
+++ C++/character_square_pattern.cpp
@@ -0,0 +1,15 @@
+// Characters Square Pattern Print Program
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Characters square pattern" << endl;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (char j = 'A'; j <= 'E'; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
